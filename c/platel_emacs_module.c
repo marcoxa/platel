50819d19063a61627c40d0521d4508309fbed317
Marco Antoniotti <marco.antoniotti@unimib.it>
2024-02-10 11:42:08 +0100

    Changed to ensure that the symbols and the functions are in place BEFORE defining the functions. Now it works as expected. Thank to Eli Zaretskii for pointing me in the right direction (yes! I should have known better).

--- c/platel_emacs_module.c
+++ c/platel_emacs_module.c
@@ -62,34 +62,34 @@ platel_e_is_little_endian(emacs_env * env,
 int
 emacs_module_init(struct emacs_runtime * er) {
 
-  emacs_env*  eenv;
-  emacs_value is_be_symbol;
-  emacs_value is_le_symbol;
-  emacs_value is_be_func;
-  emacs_value is_le_func;
-  emacs_value def_is_be_args[] = {is_be_symbol, is_be_func};
-  emacs_value def_is_le_args[] = {is_le_symbol, is_le_func};
-  
   if (er->size < sizeof(*er))
     return 1;			/* Error code. */
-
-  eenv = er->get_environment(er);
-
-  is_be_symbol = eenv->intern(eenv, "platel-is-big-endian");
-  is_le_symbol = eenv->intern(eenv, "platel-is-little-endian");
-
-  is_be_func =
-    eenv->make_function(eenv, 0, 0, platel_e_is_big_endian,
-			"Returns T if the current platform is big-endian.",
-			NULL);
-  is_le_func =
-    eenv->make_function(eenv, 0, 0, platel_e_is_little_endian,
-			"Returns T if the current platform is little-endian.",
-			NULL);
-
-  eenv->funcall(eenv, eenv->intern(eenv, "defalias"), 2, def_is_be_args);
-  eenv->funcall(eenv, eenv->intern(eenv, "defalias"), 2, def_is_le_args);
+  else {
+    emacs_env*  eenv =
+      er->get_environment(er);
   
+    emacs_value is_be_symbol =
+      eenv->intern(eenv, "platel-is-big-endian");
+  
+    emacs_value is_le_symbol =
+      eenv->intern(eenv, "platel-is-little-endian");
+    
+    emacs_value is_be_func =
+      eenv->make_function(eenv, 0, 0, platel_e_is_big_endian,
+			  "Returns T if the current platform is big-endian.",
+			  NULL);
+  
+    emacs_value is_le_func =
+      eenv->make_function(eenv, 0, 0, platel_e_is_little_endian,
+			  "Returns T if the current platform is little-endian.",
+			  NULL);
+  
+    emacs_value def_is_be_args[] = {is_be_symbol, is_be_func};
+    emacs_value def_is_le_args[] = {is_le_symbol, is_le_func};
+  
+    eenv->funcall(eenv, eenv->intern(eenv, "defalias"), 2, def_is_be_args);
+    eenv->funcall(eenv, eenv->intern(eenv, "defalias"), 2, def_is_le_args);
+  }
   return 0;			/* All is well. */
 }
 
